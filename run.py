3f286b5bd248c00453ab9264e5a2e03bdc5870a8
Vanessa <vanessa.nunez.p@gmail.com>
2018-05-29 18:13:54 -0300

    aula 9 - terminando forms

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     manager.run()
 
 
-# https://www.youtube.com/watch?v=Q_SpLfhFiK8&index=9&list=PL3BqW_m3m6a05ALSBW02qDXmfDKIip2KX
-# https://www.youtube.com/watch?v=Q_SpLfhFiK8&index=9&list=PL3BqW_m3m6a05ALSBW02qDXmfDKIip2KX
+# https://www.youtube.com/watch?v=_yTjp9IiUB0&list=PL3BqW_m3m6a05ALSBW02qDXmfDKIip2KX&index=11
