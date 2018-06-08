e1baefd370b60b46f59a0f022c0310425685bd17
Vanessa Nunes <vanessa.nunez.p@gmail.com>
2018-06-07 23:07:53 -0300

    tentando implementar login_user

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app, db
 
 from app.models.forms import LoginForm
 from app.models.tables import User
+from flask_login import login_user
 
 @app.route("/index")
 @app.route("/")
@@ -13,9 +14,16 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first
+        print(user)
+
+        import pdb; pdb.set_trace()
+        if user() and user().password == form.password.data:
+            print(form.password.data, user().password)
+            login_user(user)
+            flash("Logger in.")
     else:
+        flash("Invalid login.")
         print(form.errors)
     return render_template('login.html', form=form)
 
